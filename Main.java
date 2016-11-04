f2c3f9bf3d7bda0c58dd924d3fe9960f10f91c26
Drothknar, Destroyer of Planets <sueanne@cs.utexas.edu>
2016-11-04 13:34:20 -0500

    the first steps!

---
+++ Main.java
@@ -0,0 +1,11 @@
+import soot.*;
+import soot.options.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scene scene = Scene.v();
+        Options.v().set_soot_classpath("./");
+        Options.v().set_allow_phantom_refs(true);
+        SootClass cl = scene.loadClass("Main", SootClass.SIGNATURES);
+    }
+}
