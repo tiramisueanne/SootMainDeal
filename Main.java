781b8f21493ee840ca22d7d71bf6261d29f6857f
Drothknar, Destroyer of Planets <sueanne@cs.utexas.edu>
2016-11-10 19:28:17 -0600

    baby steps!

--- Main.java
+++ Main.java
@@ -4,7 +4,8 @@ import soot.options.*;
 class Main {
     public static void main(String[] args) {
         Scene scene = Scene.v();
-        Options.v().set_soot_classpath("./");
+        Options.v().set_soot_classpath("./:/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/rt.jar");
+        //Options.v().set_soot_classpath("./");
         Options.v().set_allow_phantom_refs(false);
         SootClass cl = scene.loadClass("Main", SootClass.SIGNATURES);
     }
