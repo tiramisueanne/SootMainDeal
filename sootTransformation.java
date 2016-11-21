86b8a846b4ff2f17541f36203c591ce74ef8a7d4
Elena Arnold <elena@cs.utexas.edu>
2016-11-21 01:05:44 -0600

    can't find main class when i try to run sootTransformation, something to do with the package name

--- sootTransformation.java
+++ sootTransformation.java
@@ -1,3 +1,5 @@
+package soot.jimple.toolkits.annotation.logic;
+
 import java.util.Iterator;
 import java.util.Map;
 
@@ -18,13 +20,26 @@ import soot.jimple.InvokeStmt;
 import soot.jimple.Jimple;
 import soot.jimple.StringConstant;
 import soot.options.Options;
-//import soot.jimple.toolkits.annotation.logic.*;
+import soot.jimple.toolkits.annotation.logic.*;
+import java.util.*;
 
 public class sootTransformation {
 	public static void main(String[] args){
 	Options.v().set_src_prec(Options.src_prec_apk);
-	Options.v().set_output_format(Options.output_format_jimple);
-	PackManager.v().getPack("jtp").add(new Transform("jtp.loopFinder", new LoopFinder()));
-	soot.Main.main(args); // or Main.main(args); ????
+        Options.v().set_allow_phantom_refs(true);
+	Options.v().set_output_format(Options.output_format_dex);
+
+        //myLoopFinder loopy =  myLoopFinder.v();
+	PackManager.v().getPack("jtp").add(new Transform("jtp.myLoopFinder", new BodyTransformer(){
+
+//            package soot.jimple.toolkits.annotation.logic;
+            @Override
+            protected void internalTransform(Body b, String phaseName, Map options){
+                LoopFinder lf = new LoopFinder();
+//                lf.internalTransform(b, phaseName, options);
+                Collection<Loop> loops = lf.loops();
+            }
+        }));
+        soot.Main.main(args); // or Main.main(args); ????
 	}
 }
