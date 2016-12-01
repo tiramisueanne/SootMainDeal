c969ae9974d08e4aef65d57ad90d1f658d8081b5
Elena Arnold <elena@cs.utexas.edu>
2016-12-01 01:21:02 -0600

    need to decide how to handle assignmentStmt, should be easy. Otherwise done

--- myLoopInstrument.java
+++ myLoopInstrument.java
@@ -46,15 +46,32 @@ public class myLoopInstrument extends BodyTransformer {
             System.out.println("Head is class: "+header.getClass());
 */
             if(header instanceof IfStmt) {
-                System.out.println("If statement found");
+                //System.out.println("If statement found");
                 IfStmt head = (IfStmt) header;
-                ConditionExpr cond = (ConditionExpr)head.getCondition();
-                System.out.println("cond class is: "+ cond.getClass());
-                Value op1 = cond.getOp1();
-                System.out.println("op1: "+op1); 
-                //this invoke stmt!
-                units.insertBefore(Jimple.v().newInvokeStmt(Jimple.v().newVirtualInvokeExpr(
-                    tmpRef, ourMethod.makeRef(), op1)), head);
+                Value cond = head.getCondition();
+                if(cond instanceof ConditionExpr){
+                    ConditionExpr condEx = (ConditionExpr)head.getCondition();
+                    //System.out.println("cond class is: "+ condEx.getClass());
+                    Value op1 = condEx.getOp1();
+                    Value op2 = condEx.getOp2(); //do I need to check if one of these is a constant?
+                    System.out.println("op1: "+op1+", op2: "+op2); 
+                    //this invoke stmt!
+                    units.insertBefore(Jimple.v().newInvokeStmt(Jimple.v().newVirtualInvokeExpr(
+                        tmpRef, ourMethod.makeRef(), op1)), head);
+                    
+                    units.insertBefore(Jimple.v().newInvokeStmt(Jimple.v().newVirtualInvokeExpr(
+                        tmpRef, ourMethod.makeRef(), op2)), head);
+                    }
+                else {
+                    System.out.println("HELP CONDITION IS NOT A CONDITIONEXPR, fix");
+                    System.out.println("cond is: "+cond.getClass()+": "+cond);
+                    System.exit(1);               
+                }
+            }
+            else {
+                System.out.println("HELP HEADER IS NOT AN IF STATEMENT, fix it");
+                System.out.println("header is: "+header.getClass()+": "+header);
+                System.exit(2);
             }
 
         }   
