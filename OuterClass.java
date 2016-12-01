53d2dce012a42a70655860d420eb5babca8d7b0e
Drothknar, Destroyer of Planets <sueanne@cs.utexas.edu>
2016-11-30 23:23:54 -0600

    fixed the static-ness

--- OuterClass.java
+++ OuterClass.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class OuterClass {
-    public static void ourMethod(Object obj) {
+    public void ourMethod(Object obj) {
         //does nothing!! Wow!
         return;
     }
